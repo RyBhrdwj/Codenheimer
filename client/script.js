cf00548263a4eb57366101e396da6da500a9ac32
thoughtlessnerd <abhay.csgo001@gmail.com>
2023-08-01 16:58:43 +0530

    visually working

--- client/script.js
+++ client/script.js
@@ -1,60 +1,63 @@
-async function checkCode()
-{
-    const inputElement = document.querySelector("input");
-    const code = inputElement.value.toLowerCase();
+const video = document.querySelector("video");
+const screen = document.querySelector(".screen");
+const lastFrame = document.querySelector("img.background");
+const launch = document.querySelector(".launch");
 
-    try {
-        const response = await fetch(`https://codenheimer-api.onrender.com/code/?code=${code}`);
-        const data = await response.json();
-        return data;
+video.addEventListener("ended", () => {
+  video.classList.add("hide");
+  lastFrame.classList.remove("hide");
+  screen.classList.remove("hide");
+});
 
-    } catch (error) {
-        const screen = document.querySelector(".screen");
-        screen.innerHTML = '';
+async function checkCode() {
+  const inputElement = document.querySelector("input");
+  const code = inputElement.value.toLowerCase();
 
-        const mssg = document.createElement("h2");
-        mssg.textContent = "An Error Occured";
-        screen.appendChild(mssg);
-    }
-}
-
-async function displayMessage()
-{
-    const isWinner = await checkCode();
+  try {
+    const response = await fetch(
+      `https://codenheimer-api.onrender.com/code/?code=${code}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
     const screen = document.querySelector(".screen");
-
-    screen.innerHTML = '';
+    screen.innerHTML = "";
 
     const mssg = document.createElement("h2");
-
-    if (isWinner === false)
-    {
-        mssg.textContent = "FUCK YOU !!!";
-    }
-    else
-    {
-        mssg.textContent = `Congratulations ! You've got ${isWinner} Place.`;
-    }
-
+    mssg.textContent = "An Error Occured";
     screen.appendChild(mssg);
+  }
+}
+
+async function displayMessage() {
+  const isWinner = await checkCode();
+  const screen = document.querySelector(".screen");
+
+  screen.innerHTML = "";
+
+  const mssg = document.createElement("h2");
+
+  if (isWinner === false) {
+    mssg.textContent = "FUCK YOU !!!";
+  } else {
+    mssg.textContent = `Congratulations ! You've got ${isWinner} Place.`;
+  }
+
+  screen.appendChild(mssg);
 }
 
 // Display message is called if either enter is pressed or when input field is completely filled.
 
-document.addEventListener("keydown", async (event) => {
-    if (event.key === "Enter")
-    {
-        event.preventDefault();
-        await displayMessage();
-    }
+launch.addEventListener("click", async (event) => {
+  await displayMessage();
 });
 
-document.addEventListener("input", (event) => {
-    const inputElement = event.target;
-    const maxLength = inputElement.getAttribute("maxlength");
-    const inputValue = inputElement.value;
-  
-    if (inputValue.length >= maxLength) {
-      displayMessage();
-    }
-});
+// document.addEventListener("input", (event) => {
+//   const inputElement = event.target;
+//   const maxLength = inputElement.getAttribute("maxlength");
+//   const inputValue = inputElement.value;
+
+//   if (inputValue.length >= maxLength) {
+//     displayMessage();
+//   }
+// });
